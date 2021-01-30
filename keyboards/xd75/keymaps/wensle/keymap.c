0b24fab874a25929e798388f91f56754d3e2aa1b
wensle <wensley.podrono@gmail.com>
2021-01-30 13:05:41 +0100

    De layout aangepast.

--- keyboards/xd75/keymaps/wensle/keymap.c
+++ keyboards/xd75/keymaps/wensle/keymap.c
@@ -28,11 +28,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
-    KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,      KC_5,           KC_6,           KC_7,           KC_8,             KC_9,            KC_0,        KC_MINUS,     KC_EQUAL,     KC_BSPC,      KC_PSCREEN,
-    KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,           KC_Y,           KC_U,           KC_I,             KC_O,            KC_P,        KC_LBRACKET,  KC_RBRACKET,  KC_BSLASH,    KC_DELETE,
-    KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,      KC_G,           KC_H,           KC_J,           KC_K,             KC_L,            KC_SCOLON,   KC_QUOTE,     KC_GRAVE,     KC_WWW_BACK,  KC_ENTER,
-    KC_LSHIFT,  KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,           KC_N,           KC_M,           KC_COMMA,         KC_DOT,          KC_SLASH,    KC_UP,        KC_HOME,      KC_END,       KC_RSHIFT,
-    KC_LCTRL,   KC_LGUI, KC_LALT, MO(_FN), KC_SPACE,  KC_LSHIFT, KC_APPLICATION, KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,  KC_LEFT,     KC_DOWN,      KC_RIGHT,     KC_LALT,      KC_RCTL
+    KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_RBRACKET, KC_HOME, KC_UP, KC_END, 
+    KC_MINUS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_LEFT, KC_DOWN, KC_RIGHT, 
+    KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_BSLASH, KC_INSERT, KC_DELETE, 
+    KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_EQUAL, KC_RSHIFT, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, 
+    KC_LCTRL, KC_LGUI, KC_LALT, KC_SPACE, MO(_FN), KC_ESC, KC_ENTER, KC_BSPC, KC_RALT, KC_APP, KC_RCTRL, MO(_FN), KC_F22, KC_F23, KC_F24
   ),
 
 /* FUNCTION
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
-    _______, KC_HOME, KC_UP, KC_END, KC_MAIL, RGB_HUD, RGB_HUI, KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,
-    _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_MSTP, RGB_SAD, RGB_SAI, KC_PLUS, _______, RESET,   _______, _______, _______, _______, _______,
-    _______, KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_PENT, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, RGB_TOG, MO(_FN), RGB_RMOD,RGB_MOD, KC_PENT, KC_PENT, MO(_FN), _______, _______, _______,  _______, _______
+    KC_PSCR, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_MS_WH_UP, _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET
   )
 };
 
